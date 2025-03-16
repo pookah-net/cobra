12056d8a8c0107867f47c8ed7ec4cd9ff5d6c5de
Ian Grant <4722050+Pookah-Productions@users.noreply.github.com>
2025-03-16 12:02:54 -0400

    Added a couple more tests.

--- command_argv_test.go
+++ command_argv_test.go
@@ -21,6 +21,38 @@ func TestArgvRailsStyle(t *testing.T) {
 	}
 }
 
+func TestArgvRailsStyle2(t *testing.T) {
+	cmdLine := []string{"db:seed"}
+
+	rootCmd := &Command{
+		Use:       "root",
+		Delimiter: RailsStyle,
+		Run:       func(_ *Command, args []string) { cmdLine = args },
+	}
+	expected := []string{"db", "seed"}
+	received := rootCmd.argv(cmdLine)
+
+	if !reflect.DeepEqual(expected, received) {
+		t.Errorf("expected command line (%s) is not the same as the received (%s)", expected, received)
+	}
+}
+
+func TestArgvRailsStyleSingle(t *testing.T) {
+	cmdLine := []string{"db"}
+
+	rootCmd := &Command{
+		Use:       "root",
+		Delimiter: RailsStyle,
+		Run:       func(_ *Command, args []string) { cmdLine = args },
+	}
+	expected := []string{"db"}
+	received := rootCmd.argv(cmdLine)
+
+	if !reflect.DeepEqual(expected, received) {
+		t.Errorf("expected command line (%s) is not the same as the received (%s)", expected, received)
+	}
+}
+
 func TestArgvStandardStyle(t *testing.T) {
 	cmdLine := []string{"db", "seed", "load"}
 
